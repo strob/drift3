ca84abf332d31c7bb95304988edef2e6f50dd46a
Robert M Ochshorn <mail@RMOZONE.COM>
2019-02-03 01:44:43 -0800

    port to python3 and add prosodic measures

---
+++ drift/measure.py
@@ -0,0 +1,228 @@
+# Measurements.
+
+from lempel_ziv_complexity import lempel_ziv_complexity
+import numpy as np
+import functools
+
+
+def smooth(y, N=10):
+    if len(y) < N:
+        return y
+
+    arr = np.array(y)
+    win = np.hanning(N)
+
+    return np.convolve(arr, win, mode="same").tolist()
+
+
+class Measure:
+    def __init__(self, pitch, alignment=None):
+        self.pitch = pitch
+        self.alignment = alignment
+
+    def _raw_measure_for_segment(self, segment, start_time=0, end_time=None):
+        out = {}
+
+        wdlist, pitch = self._trim_segment(segment, start_time, end_time)
+
+        out["pitched"] = [X for X in pitch if X > 20]
+
+        # Compute pitch velocity and acceleration deltas on smoothed
+        # voiced chunks, in log-space.
+
+        # first, get pitched chunks
+        is_pitched = False
+        pitched_chunks = []
+        cur_pitches = None
+        for p_val in pitch:
+            if is_pitched:
+                if p_val < 20:
+                    # End a pitched chunk
+                    is_pitched = False
+                else:
+                    cur_pitches.append(p_val)
+            else:
+                if p_val > 20:
+                    # Start a pitched chunk!
+                    is_pitched = True
+                    cur_pitches = [p_val]
+                    pitched_chunks.append(cur_pitches)
+
+        # then, smooth out each pitched segment
+        pitched_chunks = [smooth(ch) for ch in pitched_chunks]
+
+        # pitch_dt -
+        dt_chunks = []
+        for ch in pitched_chunks:
+            dt_chunk = []
+            dt_chunks.append(dt_chunk)
+            for idx, p1_val in enumerate(ch[:-1]):
+                p2_val = ch[idx + 1]
+                dt_chunk.append(abs(np.log2(p1_val) - np.log2(p2_val)))
+
+        # pitch_dv -
+        dv_chunks = []
+        for ch in dt_chunks:
+            dv_chunk = []
+            dv_chunks.append(dv_chunk)
+            for idx, p1_val in enumerate(ch[:-1]):
+                p2_val = ch[idx + 1]
+                dv_chunk.append(abs(p1_val - p2_val))
+
+        out["pitch_log_deltas"] = functools.reduce(lambda acc, x: acc + x, dt_chunks)
+        out["pitch_velocity_deltas"] = functools.reduce(
+            lambda acc, x: acc + x, dv_chunks
+        )
+
+        if len(wdlist) == 0:
+            return out
+
+        if end_time is None:
+            end_time = wdlist[-1]["end"]
+
+        out["duration"] = end_time - start_time
+
+        out["number_of_words"] = len([X for X in wdlist if X.get("word") is not None])
+
+        out["number_of_phonemes"] = sum([len(X.get("phones", [])) for X in wdlist])
+
+        out["number_of_long_pauses"] = len(
+            [
+                X
+                for X in wdlist
+                if X.get("type") == "gap"
+                and (X["end"] - X["start"] > 0.5)
+                and (X["end"] - X["start"] < 2)
+            ]
+        )
+
+        pauses = [
+            X
+            for X in wdlist
+            if X.get("type") == "gap"
+            and (X["end"] - X["start"] > 0.1)
+            and (X["end"] - X["start"] < 2)
+        ]
+        out["number_of_pauses"] = len(pauses)
+
+        out["pause_duration"] = sum([X["end"] - X["start"] for X in pauses])
+
+        # Based on `prosodic_measures.py`, accumulate a binary string indicating gap or non-gap, sampled at 100hz.
+
+        wd_s = ""
+        voice_s = ""
+
+        cur_t = start_time
+        cur_wd_idx = 0
+        cur_pitch_idx = 0
+        while cur_t < end_time:
+            # Word sequence
+            obj = wdlist[cur_wd_idx]
+            if obj.get("type") == "gap":
+                wd_s += "0"
+            else:
+                wd_s += "1"
+
+            # ...voiced sequence
+            if pitch[cur_pitch_idx] > 20:
+                voice_s += "1"
+            else:
+                voice_s += "0"
+
+            cur_t += 0.01
+            if len(pitch) > cur_pitch_idx + 1:
+                cur_pitch_idx += 1
+            if obj["end"] < cur_t and len(wdlist) > cur_wd_idx + 1:
+                cur_wd_idx += 1
+
+        out["word_gap_sequence"] = wd_s
+        out["voiced_gap_sequence"] = voice_s
+
+        return out
+
+    def _accumulate_stats(self, acc, s2):
+        # Does everything just sum?
+        out = {}
+        for k, v in s2.items():
+            out[k] = acc[k] + v
+        return out
+
+    def _compute_measure(self, stats):
+        out = {}
+        if stats["number_of_pauses"]:
+            out["mean_pause_duration"] = (
+                stats["pause_duration"] / stats["number_of_pauses"]
+            )
+
+        out["words_per_minute"] = stats["number_of_words"] / (stats["duration"] / 60.0)
+        out["phonemes_per_minute"] = stats["number_of_phonemes"] / (
+            stats["duration"] / 60.0
+        )
+
+        out["long_pauses_per_minute"] = stats["number_of_long_pauses"] / (
+            stats["duration"] / 60.0
+        )
+
+        # Replace "entropy" with "chroma_uniformity"
+        n_bins = 10
+        # quantize to 25 bins, per-octave
+        chroma = [int((np.log2(X) % 1) * n_bins) for X in stats["pitched"]]
+        # return the ratio of 10th to 90th percentile
+        cnts = {}
+        for c in chroma:
+            cnts[c] = cnts.get(c, 0) + 1
+        cnts = sorted(cnts.values())
+        out["octave_variation"] = cnts[-2] / max(1, cnts[1])
+
+        # filter pitch within 9-91% distribution
+        pitched = sorted(stats["pitched"])
+        trim_pt = int(len(pitched) * 0.09)
+        pitched = pitched[trim_pt:-trim_pt]
+
+        min_pitch = pitched[0]
+        max_pitch = pitched[-1]
+
+        out["pitch_range_octaves"] = np.log2(max_pitch / min_pitch)
+
+        out["log_mean_pitch_hz"] = 2 ** (
+            sum([np.log2(X) for X in pitched]) / len(pitched)
+        )
+
+        # pitch speed (octaves/sec)
+        out["pitch_speed_octaves"] = 100 * (
+            sum(stats["pitch_log_deltas"]) / len(stats["pitch_log_deltas"])
+        )
+        # pitch accel (octaves/sec^2)
+        out["pitch_acceleration"] = 100 * (
+            sum(stats["pitch_velocity_deltas"]) / len(stats["pitch_velocity_deltas"])
+        )
+
+        wd_s = stats["word_gap_sequence"]
+        voice_s = stats["voiced_gap_sequence"]
+
+        out["rhythmic_complexity_of_pauses_between_words"] = lempel_ziv_complexity(
+            wd_s
+        ) / (len(wd_s) / np.log2(len(wd_s)))
+        out[
+            "rhythmic_complexity_of_pauses_between_voiced_periods"
+        ] = lempel_ziv_complexity(voice_s) / (len(voice_s) / np.log2(len(voice_s)))
+
+        return out
+
+    def _trim_segment(self, segment, start_time, end_time):
+        # Compute measures for some or all of a segment
+        wdlist = segment["wdlist"]
+        pitch = self.pitch
+
+        # 1. Trim segment words & pitch
+        if start_time is not None:
+            wdlist = [X for X in wdlist if X["end"] > start_time]
+            pitch = pitch[int(start_time * 100) :]
+        else:
+            start_time = 0
+
+        if end_time is not None:
+            wdlist = [X for X in wdlist if X["start"] < end_time]
+            pitch = pitch[: int((end_time - start_time) * 100) + 1]
+
+        return wdlist, pitch
--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import guts
 from twisted.web.static import File
@@ -10,53 +10,68 @@ import json
 import nmt
 import numpy as np
 
-BUNDLE=False
+BUNDLE = False
+
 
 def get_ffmpeg():
     if BUNDLE:
-        return './ffmpeg'
-    return 'ffmpeg'
+        return "./ffmpeg"
+    return "ffmpeg"
+
+
 def get_local():
     if BUNDLE:
-        return os.path.join(os.environ['HOME'], '.drift3', 'local')
-    return 'local'
+        return os.path.join(os.environ["HOME"], ".drift3", "local")
+    return "local"
+
+
 def get_attachpath():
-    return os.path.join(get_local(), '_attachments')
+    return os.path.join(get_local(), "_attachments")
+
+
 def get_calc_sbpca():
     if BUNDLE:
-        return './SAcC'
-    return './ext/calc_sbpca/python/SAcC.py'
+        return "./SAcC"
+    return "./ext/calc_sbpca/python/SAcC.py"
 
 
-root = guts.Root(port=9899, interface='127.0.0.1', dirpath='www')
+root = guts.Root(port=9899, interface="127.0.0.1", dirpath="www")
 
 db = guts.Babysteps("local/db")
 
-rec_set = guts.BSFamily('recording')
-root.putChild('_rec', rec_set.res)
+rec_set = guts.BSFamily("recording")
+root.putChild(b"_rec", rec_set.res)
 
-tran_set = guts.BSFamily('transcript')
-root.putChild('_trans', tran_set.res)
+tran_set = guts.BSFamily("transcript")
+root.putChild(b"_trans", tran_set.res)
+
 
 def pitch(cmd):
-    docid = cmd['id']
+    docid = cmd["id"]
 
     meta = rec_set.get_meta(docid)
 
     # Create an 8khz wav file
-    with tempfile.NamedTemporaryFile(suffix='.wav') as wav_fp:
-        subprocess.call([get_ffmpeg(),
-                         '-y',
-                         '-loglevel', 'panic',
-                         '-i', os.path.join(get_attachpath(), meta['path']),
-                         '-ar', '8000',
-                         '-ac', '1',
-                         wav_fp.name])
+    with tempfile.NamedTemporaryFile(suffix=".wav") as wav_fp:
+        subprocess.call(
+            [
+                get_ffmpeg(),
+                "-y",
+                "-loglevel",
+                "panic",
+                "-i",
+                os.path.join(get_attachpath(), meta["path"]),
+                "-ar",
+                "8000",
+                "-ac",
+                "1",
+                wav_fp.name,
+            ]
+        )
 
         # ...and use it to compute pitch
-        with tempfile.NamedTemporaryFile(suffix='.txt', delete=False) as pitch_fp:
-            subprocess.call([get_calc_sbpca(),
-                             wav_fp.name, pitch_fp.name])
+        with tempfile.NamedTemporaryFile(suffix=".txt", delete=False) as pitch_fp:
+            subprocess.call([get_calc_sbpca(), wav_fp.name, pitch_fp.name])
 
     if len(open(pitch_fp.name).read().strip()) == 0:
         return {"error": "Pitch computation failed"}
@@ -64,32 +79,36 @@ def pitch(cmd):
     # XXX: frozen attachdir
     pitchhash = guts.attach(pitch_fp.name, get_attachpath())
 
-    guts.bschange(rec_set.dbs[docid], {
-        "type": "set",
-        "id": "meta",
-        "key": "pitch",
-        "val": pitchhash
-        })
+    guts.bschange(
+        rec_set.dbs[docid],
+        {"type": "set", "id": "meta", "key": "pitch", "val": pitchhash},
+    )
 
     return {"pitch": pitchhash}
 
-root.putChild("_pitch", guts.PostJson(pitch, async=True))
+
+root.putChild(b"_pitch", guts.PostJson(pitch, runasync=True))
+
 
 def parse_speakers_in_transcript(trans):
     segs = []
 
     cur_speaker = None
-    for line in trans.split('\n'):
-        if ':' in line and line.index(':') < 32 and len(line.split(':')[0].split(' ')) < 3:
-            cur_speaker = line.split(':')[0]
-            line = ':'.join(line.split(':')[1:])
+    for line in trans.split("\n"):
+        if (
+            ":" in line
+            and line.index(":") < 32
+            and len(line.split(":")[0].split(" ")) < 3
+        ):
+            cur_speaker = line.split(":")[0]
+            line = ":".join(line.split(":")[1:])
 
         line = line.strip()
         if len(line) > 0:
-            segs.append({'speaker': cur_speaker,
-                         'line': line})
+            segs.append({"speaker": cur_speaker, "line": line})
 
     return segs
+
 
 def gentle_punctuate(wdlist, transcript):
     # Use the punctuation from Gentle's transcript in a wdlist
@@ -98,15 +117,15 @@ def gentle_punctuate(wdlist, transcript):
     last_word_end = None
     next_aligned_wd = None
 
-    for wd_idx,wd in enumerate(wdlist):
+    for wd_idx, wd in enumerate(wdlist):
         next_wd_idx = wd_idx + 1
         next_wd = None
 
-        is_aligned = wd.get('end') is not None
+        is_aligned = wd.get("end") is not None
 
         while next_wd_idx < len(wdlist):
             next_wd = wdlist[next_wd_idx]
-            if next_wd.get('startOffset') is not None:
+            if next_wd.get("startOffset") is not None:
                 break
             next_wd_idx += 1
 
@@ -114,18 +133,18 @@ def gentle_punctuate(wdlist, transcript):
             next_wd_idx = wd_idx + 1
             while next_wd_idx < len(wdlist):
                 next_aligned_wd = wdlist[next_wd_idx]
-                if next_aligned_wd.get('end') is not None:
+                if next_aligned_wd.get("end") is not None:
                     break
                 else:
                     next_aligned_wd = None
                 next_wd_idx += 1
 
-        if next_wd is None or next_wd.get('startOffset') is None:
+        if next_wd is None or next_wd.get("startOffset") is None:
             # No next word - don't glob punctuation, just return what we have.
 
-            keys = ['start', 'end', 'phones']
+            keys = ["start", "end", "phones"]
 
-            wd_obj = {'word': wd['word']}
+            wd_obj = {"word": wd["word"]}
             for key in keys:
                 if key in wd:
                     wd_obj[key] = wd[key]
@@ -133,14 +152,14 @@ def gentle_punctuate(wdlist, transcript):
             out.append(wd_obj)
             break
 
-        if 'startOffset' not in wd:# or 'startOffset' not in next_wd:
+        if "startOffset" not in wd:  # or 'startOffset' not in next_wd:
             continue
-        if wd.get('startOffset') is not None:
-            wd_str = transcript[wd['startOffset']:next_wd['startOffset']]
+        if wd.get("startOffset") is not None:
+            wd_str = transcript[wd["startOffset"] : next_wd["startOffset"]]
 
-            keys = ['start', 'end', 'phones']
+            keys = ["start", "end", "phones"]
 
-            wd_obj = {'word': wd_str}
+            wd_obj = {"word": wd_str}
             for key in keys:
                 if key in wd:
                     wd_obj[key] = wd[key]
@@ -156,81 +175,89 @@ def gaps_and_unaligned(seq):
     cur_unaligned = []
     last_end = 0
 
-    for idx,wd in enumerate(seq):
-        if wd.get('end'):
+    for idx, wd in enumerate(seq):
+        if wd.get("end"):
             if len(cur_unaligned) > 0:
                 # End of an unaligned block
-                out.append({
-                    'type': 'unaligned',
-                    'start': last_end,
-                    'end': wd['start'],
-                    'word': ''.join([X['word'] for X in cur_unaligned])
-                    })
+                out.append(
+                    {
+                        "type": "unaligned",
+                        "start": last_end,
+                        "end": wd["start"],
+                        "word": "".join([X["word"] for X in cur_unaligned]),
+                    }
+                )
 
                 cur_unaligned = []
 
-            if len(out) > 0 and out[-1]['end'] < wd['start']:
+            if len(out) > 0 and out[-1]["end"] < wd["start"]:
                 # gap
-                out.append({
-                    'type': 'gap',
-                    'start': last_end,
-                    'end': wd['start'],
-                    'word': '[gap]'
-                })
+                out.append(
+                    {
+                        "type": "gap",
+                        "start": last_end,
+                        "end": wd["start"],
+                        "word": "[gap]",
+                    }
+                )
 
             out.append(wd)
-            last_end = wd['end']
+            last_end = wd["end"]
         else:
             # unaligned
             cur_unaligned.append(wd)
 
     if len(cur_unaligned) > 0:
         # End of an unaligned block
-        out.append({
-            'type': 'unaligned',
-            'start': last_end,
-            'word': '[%s]' % (''.join([X['word'] for X in cur_unaligned]))
-            })
+        out.append(
+            {
+                "type": "unaligned",
+                "start": last_end,
+                "word": "[%s]" % ("".join([X["word"] for X in cur_unaligned])),
+            }
+        )
 
     return out
 
+
 def align(cmd):
-    meta = rec_set.get_meta(cmd['id'])
+    meta = rec_set.get_meta(cmd["id"])
 
-    media = os.path.join(get_attachpath(), meta['path'])
+    media = os.path.join(get_attachpath(), meta["path"])
     segs = parse_speakers_in_transcript(
-        open(
-            os.path.join(get_attachpath(), meta['transcript'])).read())
+        open(os.path.join(get_attachpath(), meta["transcript"])).read()
+    )
 
-    with tempfile.NamedTemporaryFile(suffix='.txt') as txtfp:
-        txtfp.write('\n'.join([X['line'] for X in segs]))
+    with tempfile.NamedTemporaryFile(suffix=".txt") as txtfp:
+        txtfp.write("\n".join([X["line"] for X in segs]))
         txtfp.flush()
 
         print("tscript - ", txtfp.name)
         # txtfp.close()
 
-        with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as fp:
+        with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as fp:
             # XXX: Check if Gentle is running
 
-            url = 'http://localhost:8765/transcriptions?async=false'
+            url = "http://localhost:8765/transcriptions?async=false"
 
             t_opts = []
-            t_opts = ['-F', 'transcript=<%s' % (txtfp.name)]
+            t_opts = ["-F", "transcript=<%s" % (txtfp.name)]
             # Adding disfluencies may be unreliable...
             # url += '&disfluency=true'
 
             # XXX: can I count on `curl` on os x? I think so?
-            subprocess.call(['curl',
-                    '-o', fp.name,
-                    '-X', 'POST',
-                    '-F', 'audio=@%s' % (media)] + t_opts + [url])
+            subprocess.call(
+                ["curl", "-o", fp.name, "-X", "POST", "-F", "audio=@%s" % (media)]
+                + t_opts
+                + [url]
+            )
 
             trans = json.load(open(fp.name))
 
     # Re-diarize Gentle output into a sane diarization format
-    diary = {'segments': [{}]}
-    seg = diary['segments'][0]
-    seg['speaker'] = segs[0]['speaker']
+    diary = {"segments": [{}]}
+    seg = diary["segments"][0]
+    seg["speaker"] = segs[0]["speaker"]
 
     wdlist = []
     end_offset = 0
@@ -238,168 +265,165 @@ def align(cmd):
 
     cur_end = 0
 
-    for wd in trans['words']:
-        gap = trans['transcript'][end_offset:wd['startOffset']]
-        seg_idx += len(gap.split('\n'))-1
+    for wd in trans["words"]:
+        gap = trans["transcript"][end_offset : wd["startOffset"]]
+        seg_idx += len(gap.split("\n")) - 1
 
-        if '\n' in gap and len(wdlist) > 0:
+        if "\n" in gap and len(wdlist) > 0:
             # Linebreak - new segment!
-            wdlist[-1]['word'] += gap.split('\n')[0]
+            wdlist[-1]["word"] += gap.split("\n")[0]
 
-            seg['wdlist'] = gentle_punctuate(wdlist, trans['transcript'])
+            seg["wdlist"] = gentle_punctuate(wdlist, trans["transcript"])
 
             # Compute start & end
-            seg['start'] = seg['wdlist'][0].get('start', cur_end)
-            has_end = [X for X in seg['wdlist'] if X.get('end')]
+            seg["start"] = seg["wdlist"][0].get("start", cur_end)
+            has_end = [X for X in seg["wdlist"] if X.get("end")]
             if len(has_end) > 0:
-                seg['end'] = has_end[-1]['end']
+                seg["end"] = has_end[-1]["end"]
             else:
-                seg['end'] = cur_end
-            cur_end = seg['end']
+                seg["end"] = cur_end
+            cur_end = seg["end"]
 
             wdlist = []
             seg = {}
-            diary['segments'].append(seg)
+            diary["segments"].append(seg)
             if len(segs) > seg_idx:
-                seg['speaker'] = segs[seg_idx]['speaker']
+                seg["speaker"] = segs[seg_idx]["speaker"]
 
         wdlist.append(wd)
-        end_offset = wd['endOffset']
+        end_offset = wd["endOffset"]
 
-    seg['wdlist'] = gentle_punctuate(wdlist, trans['transcript'])
+    seg["wdlist"] = gentle_punctuate(wdlist, trans["transcript"])
 
     # Compute start & end
-    seg['start'] = seg['wdlist'][0].get('start', cur_end)
-    has_end = [X for X in seg['wdlist'] if X.get('end')]
+    seg["start"] = seg["wdlist"][0].get("start", cur_end)
+    has_end = [X for X in seg["wdlist"] if X.get("end")]
     if len(has_end) > 0:
-        seg['end'] = has_end[-1]['end']
+        seg["end"] = has_end[-1]["end"]
     else:
-        seg['end'] = cur_end
+        seg["end"] = cur_end
 
     # For now, hit disk. Later we can explore the transcription DB.
-    with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as dfh:
+    with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as dfh:
         json.dump(diary, dfh, indent=2)
 
         alignhash = guts.attach(dfh.name, get_attachpath())
 
-    guts.bschange(rec_set.dbs[cmd['id']], {
-        "type": "set",
-        "id": "meta",
-        "key": "align",
-        "val": alignhash
-        })
+    guts.bschange(
+        rec_set.dbs[cmd["id"]],
+        {"type": "set", "id": "meta", "key": "align", "val": alignhash},
+    )
 
     return {"align": alignhash}
 
 
-root.putChild("_align", guts.PostJson(align, async=True))
+root.putChild(b"_align", guts.PostJson(align, runasync=True))
+
 
 def gen_csv(cmd):
-    docid = cmd['id']
+    docid = cmd["id"]
     meta = rec_set.get_meta(docid)
 
-    p_path = os.path.join(get_attachpath(), meta['pitch'])
-    pitch = [float(X.split()[1]) for X in open(p_path) if len(X.split())>2]
+    p_path = os.path.join(get_attachpath(), meta["pitch"])
+    pitch = [float(X.split()[1]) for X in open(p_path) if len(X.split()) > 2]
 
-    a_path = os.path.join(get_attachpath(), meta['align'])
+    a_path = os.path.join(get_attachpath(), meta["align"])
     align = json.load(open(a_path))
 
     words = []
-    for seg in align['segments']:
-        for wd in seg['wdlist']:
+    for seg in align["segments"]:
+        for wd in seg["wdlist"]:
             wd_p = dict(wd)
-            wd_p['speaker'] = seg['speaker']
+            wd_p["speaker"] = seg["speaker"]
             words.append(wd_p)
 
-    with tempfile.NamedTemporaryFile(suffix='.csv', delete=False) as fp:
+    with tempfile.NamedTemporaryFile(suffix=".csv", delete=False) as fp:
         w = csv.writer(fp)
 
-        w.writerow(['time (s)', 'pitch (hz)', 'word', 'phoneme', 'speaker'])
+        w.writerow(["time (s)", "pitch (hz)", "word", "phoneme", "speaker"])
 
-        for idx, pitch in enumerate(pitch):
+        for idx, pitch_val in enumerate(pitch):
             t = idx / 100.0
 
             wd_txt = None
             ph_txt = None
-            speaker= None
+            speaker = None
 
             for wd_idx, wd in enumerate(words):
-                if wd.get('start') is None:
+                if wd.get("start") is None:
                     continue
 
-                if wd['start'] <= t and wd['end'] >= t:
-                    wd_txt = wd['word'].encode('utf-8')
+                if wd["start"] <= t and wd["end"] >= t:
+                    wd_txt = wd["word"].encode("utf-8")
 
-                    speaker = wd['speaker']
+                    speaker = wd["speaker"]
 
                     # find phone
-                    cur_t = wd['start']
-                    for phone in wd['phones']:
-                        if cur_t + phone['duration'] >= t:
-                            ph_txt = phone['phone']
+                    cur_t = wd["start"]
+                    for phone in wd["phones"]:
+                        if cur_t + phone["duration"] >= t:
+                            ph_txt = phone["phone"]
                             break
-                        cur_t += phone['duration']
+                        cur_t += phone["duration"]
 
                     break
 
-            row = [t, pitch, wd_txt, ph_txt, speaker]
+            row = [t, pitch_val, wd_txt, ph_txt, speaker]
             w.writerow(row)
 
         fp.flush()
 
     csvhash = guts.attach(fp.name, get_attachpath())
-    guts.bschange(rec_set.dbs[cmd['id']], {
-        "type": "set",
-        "id": "meta",
-        "key": "csv",
-        "val": csvhash
-        })
+    guts.bschange(
+        rec_set.dbs[cmd["id"]],
+        {"type": "set", "id": "meta", "key": "csv", "val": csvhash},
+    )
 
-    return {'csv': csvhash}
+    return {"csv": csvhash}
 
-root.putChild("_csv", guts.PostJson(gen_csv, async=True))
+
+root.putChild(b"_csv", guts.PostJson(gen_csv, runasync=True))
+
 
 def rms(cmd):
-    docid = cmd['id']
+    docid = cmd["id"]
     info = rec_set.get_meta(docid)
 
-    vpath = os.path.join('local', '_attachments', info['path'])
+    vpath = os.path.join("local", "_attachments", info["path"])
 
     R = 44100
 
-    snd = nmt.sound2np(vpath, R=R, nchannels=1, ffopts=['-filter:a', 'dynaudnorm'])
+    snd = nmt.sound2np(vpath, R=R, nchannels=1, ffopts=["-filter:a", "dynaudnorm"])
 
-    WIN_LEN = R/100
+    WIN_LEN = R / 100
 
     rms = []
     for idx in range(len(snd) / WIN_LEN):
-        chunk = snd[idx*WIN_LEN:(idx+1)*WIN_LEN]
+        chunk = snd[idx * WIN_LEN : (idx + 1) * WIN_LEN]
         rms.append((chunk.astype(float) ** 2).sum() / len(chunk))
     rms = np.array(rms)
 
     rms -= rms.min()
     rms /= rms.max()
 
-    with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as fh:
+    with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as fh:
         json.dump(rms.tolist(), fh)
 
         rmshash = guts.attach(fh.name, get_attachpath())
 
-    guts.bschange(rec_set.dbs[docid], {
-        "type": "set",
-        "id": "meta",
-        "key": "rms",
-        "val": rmshash
-        })
+    guts.bschange(
+        rec_set.dbs[docid], {"type": "set", "id": "meta", "key": "rms", "val": rmshash}
+    )
 
-    return {'rms': rmshash}
+    return {"rms": rmshash}
 
-root.putChild('_rms', guts.PostJson(rms, async=True))
 
-root.putChild("_db", db)
-root.putChild("_attach", guts.Attachments())
-root.putChild('_stage', guts.Codestage(wwwdir='www'))
+root.putChild(b"_rms", guts.PostJson(rms, runasync=True))
 
-root.putChild('media', File('local/_attachments'))
+root.putChild(b"_db", db)
+root.putChild(b"_attach", guts.Attachments())
+root.putChild(b"_stage", guts.Codestage(wwwdir="www"))
 
-guts.serve('stage.py', globals(), root=root)
+root.putChild(b"media", File("local/_attachments"))
+
+guts.serve("stage.py", globals(), root=root)
